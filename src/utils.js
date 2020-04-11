5491d0ac3b21e42f0068e5a3e75ea266ef91a851
Samuel Mumo <samumo.dev@gmail.com>
2020-04-11 21:01:19 +0300

    Fix

--- src/utils.js
+++ src/utils.js
@@ -42,7 +42,7 @@ const getDollarsInFlight = (
   population,
   income,
   days
-) => Math.trunc((infectionsByRequestedTime * population * income) / days);
+) => Math.trunc(((infectionsByRequestedTime * population * income) / days) * 10);
 
 export {
   getDollarsInFlight,
