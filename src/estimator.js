80c77cdb6a28fbd4697dcf2ec722d7e68726c33d
Samuel Mumo <samumo.dev@gmail.com>
2020-04-14 20:07:55 +0300

    clean up

--- src/estimator.js
+++ src/estimator.js
@@ -147,20 +147,3 @@ const covid19ImpactEstimator = (data) => {
 };
 
 export default covid19ImpactEstimator;
-
-// const data = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 4,
-//     avgDailyIncomePopulation: 0.73
-//   },
-//   periodType: 'days',
-//   timeToElapse: 38,
-//   reportedCases: 2747,
-//   population: 92931687,
-//   totalHospitalBeds: 678874
-// };
-
-// // eslint-disable-next-line no-console
-// console.log(covid19ImpactEstimator(data));
